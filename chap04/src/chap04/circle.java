eb0d808b21582e2c7df633af9aed2b2ec115b747
USER <USER@AMP-07>
2019-03-20 16:37:22 +0900

    ?

---
+++ chap04/src/chap04/circle.java
@@ -0,0 +1,26 @@
+package chap04;
+
+public class circle {
+	int re;
+	string name;
+	
+	public circle()
+	{
+		
+	}
+	public double get Area(){
+		return 3.14*re*re;
+		
+	}
+	public static void main(String[] args)
+	{
+		circle pizza;
+		pizza = new circle();
+		pizza.name="ÇÇÀÚ";
+		pizza.re=5;
+		double area = pizza.getArea();
+		system
+				
+				
+	}
+}
