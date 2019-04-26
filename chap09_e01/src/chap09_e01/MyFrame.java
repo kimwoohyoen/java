91f336d4b59c2cedad35454e8cc4930a28a7c892
USER <USER@AMP-08>
2019-04-26 09:53:17 +0900

    commit

---
+++ chap09_e01/src/chap09_e01/MyFrame.java
@@ -0,0 +1,17 @@
+package chap09_e01;
+
+import javax.swing.JFrame;
+
+public class MyFrame extends JFrame {
+	    public MyFrame() {
+	    	   setTitle("±è¿ìÇö");
+	    	   setSize(300, 300);
+	   		   setVisible(true);
+	    }
+	
+	public static void main(String[] args) {
+		
+		    MyFrame frame = new MyFrame();
+	}
+
+}
